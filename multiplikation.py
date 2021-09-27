0f8d0a37076e78d6d35f62db63b0c0bf1ddee3b9
Jesper Svensson <jesper.svensson00@hotmail.com>
2021-09-27 16:07:14 +0200

    Första

---
+++ multiplikation.py
@@ -0,0 +1,11 @@
+def main():
+    # Skriver ut en multiplikationstabell
+    n = 9
+    print()
+    print("Multiplikationstabell för", n)
+    print("---------------------------")
+    for i in range(1, 10):
+        print(i*n)
+    print()
+
+main()
