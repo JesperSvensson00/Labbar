37a482ac74982a17ba9a256d1a18007ace06a029
JesperSvensson00 <jesper.svensson00@hotmail.com>
2021-10-03 20:39:45 +0200

    Började med programmet

---
+++ Labb1/main.py
@@ -0,0 +1,9 @@
+# Jonas & Jesper 2021-10-04
+
+PRIS_ENKEL = 24
+PRIS_30DAGAR = 640
+
+antal_resor = int(
+    input('Hur många resor kommer du göra under 30 dagars perioden? (Ange heltal)'))
+
+print(antal_resor)
