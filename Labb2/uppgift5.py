0e8b7d2b3dc6ba3200c848fd21c4629b02f7370c
JesperSvensson00 <jesper.svensson00@hotmail.com>
2021-10-12 13:13:25 +0200

    La till kommentarer

--- Labb2/uppgift5.py
+++ Labb2/uppgift5.py
@@ -1,40 +1,46 @@
 import random
 
-
+# Skriver ut info om hur långe ne foot är i meter
 def foot():
     foot = 30.48
     print("En foot är", foot, "cm.")
 
 
+# Omvandlar inputvärdet i meter till foot och returnera det
 def meter_till_foot(längd):
     foot = 0.3048
     svar = längd * foot
     return svar
 
 
+# Skriver ut en multiplikations tabell förett angivet värde
 def skriv_ut_tabell():
     antal_gånger = int(input("Hur många gånger vill du köra loopen? "))
     for i in range(1, antal_gånger+1):
         print(meter_till_foot(i))
 
 
+# Denna funktionen slumpar en längd i meter och låter användaren gissa på vad det blir i foot
 def gissa():
+    # Slumpar ett tal mellan 1 och 10 och skriver ut det
     tal = random.randint(1, 10)
     print("Antal meter:", tal)
 
     antal_försök = 1
-    while True:
+    while True:  # Loopar till användaren svara rätt
+        # Omvandlar värdet till en float
         svar = float(input("Skriv vad det blir i foot: "))
         if svar == meter_till_foot(tal):
             print("Rätt!")
             print("Du gjorde", antal_försök, "försök.")
-            break
+            break  # Avbryter loopen
         else:
             print("Fel!")
 
         antal_försök += 1
 
 
+# Kör alla funktioner
 def main():
     foot()
     skriv_ut_tabell()
